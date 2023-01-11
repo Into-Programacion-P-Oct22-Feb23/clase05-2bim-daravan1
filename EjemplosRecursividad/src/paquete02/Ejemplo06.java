e9e21a2a90364c2b32a28d3f3cda2048c16b3d2e
Dara <dvan1@utpl.edu.ec>
2023-01-11 09:59:00 -0500

    Primer commit

--- EjemplosRecursividad/src/paquete02/Ejemplo06.java
+++ EjemplosRecursividad/src/paquete02/Ejemplo06.java
@@ -10,7 +10,25 @@ public class Ejemplo06 {
 
         int[] valor1 = {100, 1000, 200, 2000, 3000};
         int[] valor2 = {1, 2, 3, 4, 5};
-        
+
+        for (int i = 0; i < valor1.length; i++) {       //opcion 1
+            System.out.printf("El valor es: %d\n",
+                    obtenerSuma(valor1[i], valor2[i]));
+        }
+        /*for (int i = 0; i < valor1.length; i++) {      //opcion 2
+            int num1 = valor1[i];
+            int num2 =  valor2[i];
+            System.out.printf("El valor es: %d\n",
+                    obtenerSuma(num1, num2));
+        }
+         
+        for (int i = 0; i < valor1.length; i++) {       //opcion 3
+            int num1 = valor1[i];
+            int num2 =  valor2[i];
+            int suma = obtenerSuma(num1, num2);
+            System.out.printf("El valor es: %d\n",
+                    suma);
+        } */
         // Encontrar la suma de las posiciones
         // [0][0]
         // [1][1]
@@ -30,4 +48,4 @@ public class Ejemplo06 {
         }
     }
 
-}
+                   }
